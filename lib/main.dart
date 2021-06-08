824d3b603f943319bd44304fdc0812f1225be53e
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-06-08 15:29:55 +0700

    scafold flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('hello world'),
+    home: Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('I Am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+            'https://wibumedia.com/wp-content/uploads/2021/03/Who-is-tao.jpg',
+          ),
+        ),
+      ),
     ),
   ));
 }
